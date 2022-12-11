45985d3ad656290d8ca9101e80c90bb72fad42d8
Java-12-Aero <seaposeidon31accts@gmail.com>
2022-12-10 18:47:54 -0600

    really changing things around now

--- src/App3.java
+++ src/App3.java
@@ -1,6 +1,10 @@
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
 import com.github.luben.zstd.ZstdInputStream;
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonParser;
@@ -11,15 +15,23 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.PreparedStatement;
 public class App3 {
-    public static void main(String[] args) throws Exception{
+    static Connection connectDB(){
         Connection conn = null;
         try { 
             String url = "jdbc:sqlite:F:/SQLlite/Databases/Destiny/trials_db.db"; //local db connect
             conn = DriverManager.getConnection(url);
         } catch (SQLException e) {System.out.println(e.getMessage());}
-        File file = new File("E:\\QBit Torrents\\PGCRS\\bungo-pgcr\\9980000000-9990000000.jsonl.zst"); //actual file: jsonl file compressed with ZStandard, each new JSON object separated by newline, no newline operators in JSON object
-        InputStream stream = new FileInputStream(file);
+        return conn;
+    }
+    static ZstdInputStream getZstdStream(String filePath) throws FileNotFoundException, IOException{
+        File file = new File(filePath);
+        InputStream stream = new FileInputStream(file); //actual file: jsonl file compressed with ZStandard, each new JSON object separated by newline, no newline operators in JSON object
         ZstdInputStream stream2 = new ZstdInputStream(stream); //only takes inputstream, overrides the inputstream object
+        return stream2;
+    }
+    public static void main(String[] args) throws Exception{
+        Connection conn = connectDB();
+        ZstdInputStream stream2 = getZstdStream("E:\\QBit Torrents\\PGCRS\\bungo-pgcr\\9980000000-9990000000.jsonl.zst");
         JsonFactory jFactory = new JsonFactory(); //for actually parsing the JSON
         int redScore = 0;
         int blueScore = 0;
@@ -28,6 +40,8 @@ public class App3 {
         int r=0;
         int batch = 0;
         String outStr = "";
+        long startTime;
+        long endTime;
         Statement stmt = conn.createStatement(); //sql statement handling
         try{
             stmt.executeQuery("PRAGMA journal_mode = WAL");
@@ -40,66 +54,13 @@ public class App3 {
         conn.setAutoCommit(false); //lets me do batch adds for sql
         System.out.println("Started");
         while((r=stream2.read()) != -1){
-            //long startTime = System.currentTimeMillis();
-            outStr = outStr + (char)r;
-            if((char)r == '\n'){
-                JsonParser jParser = jFactory.createParser(outStr); //allows the parser to move through the finalized string
-                while(jParser.nextToken() != null){
-                    if("_id".equals(jParser.getCurrentName())){ 
-                        jParser.nextToken();
-                        id = jParser.getValueAsLong();
-                    }
-                    if("mode".equals(jParser.getCurrentName())){
-                        jParser.nextToken();
-                        if(jParser.getValueAsInt() == 84){
-                            mode = 84;
-                        } else{
-                            mode = -1;
-                        }
-                    }
-                    if("players".equals(jParser.getCurrentName())){
-                        jParser.skipChildren(); //skips data I don't care about when parsing
-                    }
-                    if(mode == 84){
-                        if("score".equals(jParser.getCurrentName())){
-                            jParser.nextToken();
-                            if(blueScore == 0){
-                                blueScore = jParser.getValueAsInt();
-                            } else {
-                                redScore = jParser.getValueAsInt();
-                            }
-                        }
-                    } else if(mode==-1){
-                        jParser.skipChildren(); //skips data I don't care about when parsing
-                    }
-                }
-                if(mode == 84){
-                    pstmt.setLong(1, id);
-                    pstmt.setInt(2,blueScore);
-                    pstmt.setInt(3,redScore);
-                    pstmt.addBatch(); //creates the statement then adds to batch
-                    batch++;
-                    //System.out.println(batch);
-                    if(batch % 100 == 0){ //executes batch
-                        System.out.println(id);
-                        try{
-                            System.out.println("Writing batch");
-                            pstmt.executeBatch();
-                            conn.commit();
-                            System.out.println("Batch Executed");
-                        } catch (SQLException e) {
-                            System.out.println(e);
-                        }
-                    }
-                }
-                //long endTime = System.currentTimeMillis();
-                //System.out.println(endTime-startTime+"ms");
-                //cleanup stuff
-                blueScore = 0;
-                redScore = 0;
-                mode = 0;
-                outStr = "";
-                id = 0L;
+            startTime = System.currentTimeMillis();
+            //outStr = outStr + (char)r;
+            System.out.println((char)r);
+            endTime = System.currentTimeMillis();
+            if(endTime-startTime > 5){
+                System.out.println("Stalled!");
+                Thread.sleep(10000);
             }
         }
         stream2.close();
